9241c8a4de51be0a09db07f42aad3f112b43f295
bonino <dario.bonino@gmail.com>
2016-05-27 07:32:28 +0200

    Done some polishing

--- org.doggateway.drivers.bluetooth.ble.gateway/src/org/doggateway/drivers/bluetooth/ble/gateway/BLEAdapterDriverInstance.java
+++ org.doggateway.drivers.bluetooth.ble.gateway/src/org/doggateway/drivers/bluetooth/ble/gateway/BLEAdapterDriverInstance.java
@@ -1,5 +1,19 @@
-/**
+/*
+ * Dog - Bluetooth Low Energy Adapter driver
  * 
+ * Copyright (c) 2016 Dario Bonino 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
  */
 package org.doggateway.drivers.bluetooth.ble.gateway;
 
@@ -19,7 +33,7 @@ import it.polito.elite.dog.core.library.model.statevalue.IdleStateValue;
 import it.polito.elite.dog.core.library.util.LogHelper;
 
 /**
- * @author bonino
+ * @author <a href="mailto:dario.bonino@gmail.com">Dario Bonino</a>
  *
  */
 public class BLEAdapterDriverInstance extends BLEDriverInstance
